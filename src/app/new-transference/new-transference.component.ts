d824884cc789d54a398a56907bac453991882b88
Joao Pedro Machado <joao.machado0601@gmail.com>
2022-02-21 08:57:39 -0300

    Creating first component

---
+++ src/app/new-transference/new-transference.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-new-transference',
+    templateUrl: './new-transference.component.html',
+    styleUrls: [
+        './new-transference.component.scss'
+    ]
+})
+export class NewTranferenceComponent {
+
+}
